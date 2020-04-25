9a2da02a46c5a33cb68d92e1ef9c31340502c062
Jánoky András <janoky.andras@nextval.hu>
2020-04-25 21:37:50 +0200

    build fix

--- GameLister/app/src/main/java/com/example/gamelister/interactor/GamesInteractor.java
+++ GameLister/app/src/main/java/com/example/gamelister/interactor/GamesInteractor.java
@@ -2,16 +2,8 @@ package com.example.gamelister.interactor;
 
 import com.example.gamelister.network.GamesApi;
 
-import javax.inject.Inject;
-
 public class GamesInteractor {
 
     GamesApi gamesApi;
-
-    @Inject
-    public GamesInteractor(GamesApi gamesApi) {
-        this.gamesApi = gamesApi;
-        SpotifyDemoApplication.injector.inject(this);
-    }
 
 }
